ea03e8701f4e65f81af768516df3682a04f4d102
sheedgecast <she@edgecast.com>
2015-02-11 13:42:51 -0800

    updated

---
+++ test4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="test4.MvcApplication" Language="C#" %>
---
+++ test4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(test4.Startup))]
+namespace test4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
